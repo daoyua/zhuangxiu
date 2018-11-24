da5b327bcedc7ff09afd43c5d50f1220fecc440b
zhouyang <814575934@qq.com>
2018-11-24 10:35:09 +0800

    版本判断大于7

--- app/src/main/java/com/zx/zhuangxiu/UpdateUtils.java
+++ app/src/main/java/com/zx/zhuangxiu/UpdateUtils.java
@@ -166,7 +166,7 @@ public class UpdateUtils {
         Log.e("apkPath:", apkPath);
         Intent intent = new Intent(Intent.ACTION_VIEW);
         //版本在7.0以上是不能直接通过uri访问的
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             File file = (new File(apkPath)); // 由于没有在Activity环境下启动Activity,设置下面的标签
 //            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             //参数1 上下文, 参数2 Provider主机地址 和配置文件中保持一致   参数3  共享的文件
